80d09912d44361c1cb828dd53a0dd0b117f0a109
Rodagoal <hernan.gn17@gmail.com>
2022-03-16 11:34:02 -0600

    Enter

--- frontend/src/Pages/Login/Login.js
+++ frontend/src/Pages/Login/Login.js
@@ -46,7 +46,7 @@ state={
                 
                 <div>
                     <form onSubmit ={this.handleSubmit}>
-                        <input type='email' name='email' placeholder='email...' required onChange={this.handleChange}></input>
+                        <input type='user' name='user' placeholder='user...' required onChange={this.handleChange}></input>
                         <input type='password' name='pwd' placeholder='password...' required onChange={this.handleChange}></input>
 
                         <button onSubmit ={this.handleSubmit}>Log in</button>
