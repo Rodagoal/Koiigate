71d92d4c7facc39af4bce9fef154b274769b46b8
Rodagoal <hernan.gn17@gmail.com>
2022-03-15 22:23:18 -0600

    Enter

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,26 @@
+ export const MenuItems=[
+    {
+        title:'Home',
+        url: '#',
+        cName: 'nav-links'
+
+    },
+    {
+        title:'Reportes',
+        url: '#',
+        cName: 'nav-links'
+
+    },
+    {
+        title:'Progreso',
+        url: '#',
+        cName: 'nav-links'
+
+    },
+    {
+        title:'Perfil',
+        url: '#',
+        cName: 'nav-links'
+
+    },
+]
